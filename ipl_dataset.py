745efdb25900246584d306aa59f8163f04cae0cc
Jyotirmoy Mandal <subho1301@gmail.com>
2018-09-07 15:22:26 +0530

    added .editorconfig and updated .gitignore

--- ipl_dataset.py
+++ ipl_dataset.py
@@ -1,3 +1,10 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy
 import csv
+
+matches_dataset = 'matches.csv'
+matches_data = open(matches_dataset, 'rt')
+reader = csv.reader(matches_data, delimiter=',', quoting=csv.QUOTE_NONE)
+x = list(reader)
+data = numpy.array(x).astype('float')
+print(data.shape)
